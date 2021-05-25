bbd0508bf6c9ba1b2b97ae5527f25d9ef99bb37d
unknown <danhaziza11@gmail.com>
2021-05-25 16:11:14 +0200

    derniere modification

--- config/db.js
+++ config/db.js
@@ -1,13 +1,11 @@
 const mysql = require("mysql2");
 const connection = mysql.createConnection({ // on utilise la connection a la bdd mysql depuis localhost avec l'user root , le password root a la database gsb
   connectionLimit: 5,
-  host     : 'cj654063-002.dbaas.ovh.net',
-  port     : 35305,
-  user     : 'gsbgarcia',
-  password : 'gsbgarciaSU2020',
-  database : 'gsbgarcia'
-  
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'gsb'
+
 });
 
 module.exports = connection   // ON EXPORTE CETTE CONNEXION POUR POUVOIR L UTILISER AILLEURS DANS NOTRE PROJET
- 
